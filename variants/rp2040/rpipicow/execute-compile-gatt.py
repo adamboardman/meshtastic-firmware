9be6abc92c315d365c4b6841de4cee8356bd3ca7
Adam Boardman <adamboardman@gmail.com>
2025-07-24 19:07:32 +0100

    Improved script so it only re-generates the header if the gatt file has been modified

--- variants/rp2040/rpipicow/execute-compile-gatt.py
+++ variants/rp2040/rpipicow/execute-compile-gatt.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import sys
-from os.path import join
+import os
 import subprocess
 import json
 import re
 
 Import("env")
 
-env.Execute("~/.platformio/packages/framework-arduinopico/pico-sdk/lib/btstack/tool/compile_gatt.py "
+if os.path.getmtime("src/platform/rp2xx0/meshtastic-profile.gatt") > os.path.getmtime("src/platform/rp2xx0/include/meshtastic-profile.h"):
+    env.Execute("~/.platformio/packages/framework-arduinopico/pico-sdk/lib/btstack/tool/compile_gatt.py "
             " src/platform/rp2xx0/meshtastic-profile.gatt"
             " src/platform/rp2xx0/include/meshtastic-profile.h")
